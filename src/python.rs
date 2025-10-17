53c5ab128f7159669bce4a34a307134bcd1305b8
Luke Street <luke@street.dev>
2025-10-17 13:44:58 -0600

    Assortment of attempted fixes

--- src/python.rs
+++ src/python.rs
@@ -1,16 +1,35 @@
-use pyo3::prelude::*;
-use pyo3::exceptions::PyException;
+use pyo3::{exceptions::PyException, prelude::*};
 
 use crate::{compress, decompress, Error};
 
 pyo3::create_exception!(lzokay, LzokayError, PyException, "Any kind of error.");
 
 // Custom Python exception classes for each lzokay::Error variant
-pyo3::create_exception!(lzokay, LookbehindOverrunError, LzokayError, "Likely indicates bad compressed LZO input.");
-pyo3::create_exception!(lzokay, OutputOverrunError, LzokayError, "Output buffer was not large enough to store the compression/decompression result.");
-pyo3::create_exception!(lzokay, InputOverrunError, LzokayError, "Compressed input buffer is invalid or truncated.");
+pyo3::create_exception!(
+    lzokay,
+    LookbehindOverrunError,
+    LzokayError,
+    "Likely indicates bad compressed LZO input."
+);
+pyo3::create_exception!(
+    lzokay,
+    OutputOverrunError,
+    LzokayError,
+    "Output buffer was not large enough to store the compression/decompression result."
+);
+pyo3::create_exception!(
+    lzokay,
+    InputOverrunError,
+    LzokayError,
+    "Compressed input buffer is invalid or truncated."
+);
 pyo3::create_exception!(lzokay, LzokayUnknownError, LzokayError, "Unknown error.");
-pyo3::create_exception!(lzokay, InputNotConsumedError, LzokayError, "Decompression succeeded, but input buffer has remaining data.");
+pyo3::create_exception!(
+    lzokay,
+    InputNotConsumedError,
+    LzokayError,
+    "Decompression succeeded, but input buffer has remaining data."
+);
 
 // Helper function to convert lzokay::Error to appropriate Python exception
 fn lzokay_error_to_pyerr(error: Error) -> PyErr {
@@ -24,32 +43,31 @@ fn lzokay_error_to_pyerr(error: Error) -> PyErr {
 }
 
 /// Decompress
-#[pyfunction(name="decompress")]
+#[pyfunction(name = "decompress")]
 fn py_decompress(data: &[u8], buffer_size: usize) -> PyResult<Vec<u8>> {
     let mut dst = vec![0u8; buffer_size];
-
     decompress::decompress(data, &mut dst).map_err(lzokay_error_to_pyerr)?;
-
     Ok(dst)
 }
 
 /// Compress data using LZO compression.
-#[pyfunction(name="compress")]
+#[pyfunction(name = "compress")]
 fn py_compress(data: &[u8]) -> PyResult<Vec<u8>> {
     compress::compress(data).map_err(lzokay_error_to_pyerr)
 }
 
 /// Returns the worst-case size for LZO compression of data of given length.
-#[pyfunction(name="compress_worst_size")]
+#[pyfunction(name = "compress_worst_size")]
 fn py_compress_worst_size(length: usize) -> PyResult<usize> {
     Ok(compress::compress_worst_size(length))
 }
 
+#[pymodule(gil_used = false)]
 pub fn lzokay(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(py_decompress, m)?)?;
     m.add_function(wrap_pyfunction!(py_compress, m)?)?;
     m.add_function(wrap_pyfunction!(py_compress_worst_size, m)?)?;
-    
+
     // Add exception classes to the module
     m.add("LzokayError", m.py().get_type::<LzokayError>())?;
     m.add("LookbehindOverrunError", m.py().get_type::<LookbehindOverrunError>())?;
@@ -57,6 +75,6 @@ pub fn lzokay(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add("InputOverrunError", m.py().get_type::<InputOverrunError>())?;
     m.add("LzokayUnknownError", m.py().get_type::<LzokayUnknownError>())?;
     m.add("InputNotConsumedError", m.py().get_type::<InputNotConsumedError>())?;
-    
+
     Ok(())
 }
